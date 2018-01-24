d2f2e074c424795f68e97dba1ef2f951505a7627
Bryan Clover <bclover09@gmail.com>
2018-01-24 16:56:56 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
 
-  state = {
-    username: 'bclover'
-  }
-
-  inputChangeHandler = (evt) => {
-    console.log('inputChangeHandler triggered!', evt);
-    this.setState({username: evt.target.value});
-  }
   render() {
     return (
       <div className="App">
-          <UserInput change={this.inputChangeHandler} name={this.state.username}/>
-          <UserOutput user={this.state.username}/>
-          <UserOutput user={this.state.username}/>
+        testing
       </div>
     );
   }
